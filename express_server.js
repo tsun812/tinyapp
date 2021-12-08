00edcebcad8a204594e7ac6e2f8473fac8ce92ce
Yan Sun <tsun812@gmail.com>
2021-12-07 20:14:55 -0800

    add login and logout

--- express_server.js
+++ express_server.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const users = {};
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  //console.log(users)
+  const templateVars = { username: users.username, urls: urlDatabase };
+  //console.log(templateVars)
   res.render("urls_index", templateVars);
 });
 
@@ -23,7 +28,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = { username: users.username, shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
 });
 
@@ -43,12 +48,26 @@ app.get("/u/:shortURL", (req, res) => {
 
 app.post("/urls", (req, res) => {
   const longURL = req.body.longURL;
-  console.log(req.body)
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = longURL;
   res.redirect("/u/"+shortURL);         // Respond with 'Ok' (we will replace this)
 
 });
+
+app.post("/urls/login", (req, res) => {
+  const username = req.body.username;
+  users.username = username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+})
+
+app.post("/urls/logout", (req, res) => {
+  console.log(req.cookies)
+  delete users.username;
+  res.clearCookie("username");
+  console.log(req.cookies)
+  res.redirect("/urls");
+})
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
